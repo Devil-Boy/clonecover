9a42f7c82b2f8d3b9634bdd2405cb645dc44c59d
Devil boy <kws512@hotmail.com>
2012-05-26 22:29:27 -0700

    Coded to work

--- src/pgDev/bukkit/CloneCover/CCMainListener.java
+++ src/pgDev/bukkit/CloneCover/CCMainListener.java
@@ -1,8 +1,12 @@
 package pgDev.bukkit.CloneCover;
 
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.*;
 import org.bukkit.event.player.PlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+
+import pgDev.bukkit.DisguiseCraft.Disguise;
 
 public class CCMainListener implements Listener {
 	final CloneCover plugin;
@@ -13,11 +17,30 @@ public class CCMainListener implements Listener {
 	
 	@EventHandler
 	public void onJoin(PlayerJoinEvent event) {
-		
+		Player player = event.getPlayer();
+		if (!plugin.hasPermissions(player, "clonecover.exempt")) {
+			Disguise disguise = new Disguise(plugin.dcAPI.newEntityID(), plugin.pluginSettings.disguiseTo, null);
+			if (plugin.dcAPI.isDisguised(player)) {
+				plugin.dcAPI.changePlayerDisguise(player, disguise);
+			} else {
+				plugin.dcAPI.disguisePlayer(player, disguise);
+			}
+			if (!plugin.pluginSettings.disguiseNotification.equalsIgnoreCase("none")) {
+				player.sendMessage(plugin.pluginSettings.disguiseNotification);
+			}
+		}
 	}
 	
 	@EventHandler
 	public void onChat(PlayerChatEvent event) {
-		
+		for (String command : plugin.pluginSettings.cancelCommands) {
+			if (event.getMessage().startsWith(command)) {
+				if (plugin.dcAPI.isDisguised(event.getPlayer())) {
+					plugin.dcAPI.undisguisePlayer(event.getPlayer());
+					event.getPlayer().sendMessage(ChatColor.GOLD + "You are no longer a clone.");
+				}
+				return;
+			}
+		}
 	}
 }
